e69aef1672ff9a862af8878775f4d6936e88b99c
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-06-16 17:03:13 +0200

    Aktualisiere main.blocks, led5x5.ts

--- led5x5.ts
+++ led5x5.ts
@@ -1,47 +1,44 @@
 
 namespace radio { // led5x5.ts
 
-
+    // zeigt Funkgruppe oder i²C Adresse im 5x5 Display binär an
 
     //% group="25 LED" subcategory="Bluetooth"
     //% block="BCD Zahl %int anzeigen ←x %x" weight=6
     //% x.min=0 x.max=4 x.defl=4
     export function plotBCD(int: number, x: number) {
-        let bi: number = Math.trunc(int)
-        while (bi > 0 && between(x, 0, 4)) {
-            plot25LED(bi % 10, x)
-            bi = Math.idiv(bi, 10)
+        int = Math.imul(int, 1) // 32 bit signed integer
+        while (int > 0 && between(x, 0, 4)) {
+            plot25LED(int % 10, x)
+            int = Math.idiv(int, 10) // 32 bit signed integer
             x--
         }
     }
-
 
     //% group="25 LED" subcategory="Bluetooth"
     //% block="HEX Zahl %int anzeigen ←x %x" weight=6
     //% x.min=0 x.max=4 x.defl=4
     export function plotHEX(int: number, x: number) {
-        let bi: number = Math.trunc(int)
-        while (bi > 0 && between(x, 0, 4)) {
-            plot25LED(bi % 16, x)
-            bi = bi >> 4
+        int = Math.imul(int, 1) // 32 bit signed integer
+        while (int > 0 && between(x, 0, 4)) {
+            plot25LED(int % 16, x)
+            int = int >> 4 // bitweise Division durch 16
             x--
         }
     }
 
-
     //% group="25 LED" subcategory="Bluetooth"
-    //% block="BIN Ziffer %int anzeigen x %x" weight=2
+    //% block="BIN 0..31 %int anzeigen x %x" weight=2
     //% x.min=0 x.max=4 x.defl=4
     export function plot25LED(int: number, x: number) {
+        int = Math.imul(int, 1) // 32 bit signed integer
         if (between(x, 0, 4)) {
-            let bi: number = Math.trunc(int)
             for (let y = 4; y >= 0; y--) {
-                if ((bi % 2) == 1) { led.plot(x, y) } else { led.unplot(x, y) }
-                bi = bi >> 1
+                if ((int % 2) == 1) { led.plot(x, y) } else { led.unplot(x, y) }
+                int = int >> 1 // bitweise Division durch 2
             }
         }
     }
-
 
 
 } // led5x5.ts
