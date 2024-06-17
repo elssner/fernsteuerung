d9af566794ee4e0e5c38fc84686c0043c4ea77d8
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-06-17 13:53:40 +0200

    Aktualisiere multiswitch.ts

--- multiswitch.ts
+++ multiswitch.ts
@@ -1,12 +1,12 @@
 
 namespace radio { // multiswitch.ts
 
-    const i2cgroveMultiswitch_x03 = 0x03
+    const i2cGroveMultiswitch_x03 = 0x03
     const i2c_CMD_GET_DEV_EVENT = 0x01	// gets device event status
 
     export enum eStatus {
         //% block="[0] nicht angeschlossen"
-        fehler = 0,
+        nicht_angeschlossen = 0,
         //% block="[5] Fahren und Lenken"
         fahren = 5,
         //% block="[1] Seilrolle und Drehkranz"
@@ -16,26 +16,63 @@ namespace radio { // multiswitch.ts
         //% block="[2] Magnet aus"
         links = 2,
         //% block="[4] Magnet an"
-        rechts = 4
+        rechts = 4,
+        fehler
     }
 
     //let n_rgb = basic.rgb(7, 7, 7)
-    let n_Status_changed = true
-    let n_Status = eStatus.fehler
+    let n_Status_changed = false
+    let n_Status: eStatus // NaN = eStatus.fehler
     let n_Magnet = false
 
     //% group="Grove Multiswitch 0x03" subcategory="Sender" color=#003F7F
     //% block="Schalter einlesen" weight=8
+    export function i2cReadSwitch() {
+        if (n_Status != eStatus.nicht_angeschlossen) {
+            if (pins.i2cWriteBuffer(i2cGroveMultiswitch_x03, Buffer.fromArray([i2c_CMD_GET_DEV_EVENT])) != 0) {
+                n_Status = eStatus.nicht_angeschlossen // switch nicht mehr abfragen
+            } else {
+                let bu = pins.i2cReadBuffer(i2cGroveMultiswitch_x03, 10) // 4 Byte + 6 Schalter = 10
+                // Byte 0-3: 32 Bit UInt32LE; Byte 4:Schalter 1 ... Byte 9:Schalter 6
+                // Byte 4-9: 00000001:Schalter OFF; 00000000:Schalter ON; Bit 1-7 löschen & 0x01
+                // Richtung N = 1, W = 2, S = 3, O = 4, M = 5
+                // ON=00000000 OFF=00000001
+
+                if (bu[3 + eStatus.fahren] == 0) {
+                    n_Status_changed = (n_Status != eStatus.fahren)
+                    n_Status = eStatus.fahren
+                } else if (bu[3 + eStatus.seilrolle] == 0) {
+                    n_Status_changed = (n_Status != eStatus.seilrolle)
+                    n_Status = eStatus.seilrolle
+                } else if (bu[3 + eStatus.zahnstange] == 0) {
+                    n_Status_changed = (n_Status != eStatus.zahnstange)
+                    n_Status = eStatus.zahnstange
+                } else if (bu[3 + eStatus.links] == 0) {
+                    n_Magnet = false
+                } else if (bu[3 + eStatus.rechts] == 0) {
+                    n_Magnet = true
+                }
+            }
+        } else { } // nicht_angeschlossen nichts machen
+
+    }
+
+
+
+    //% group="Grove Multiswitch 0x03" subcategory="Sender" color=#003F7F deprecated=true
+    //% block="Schalter einlesen0" weight=8
     export function readSwitch(): boolean {
-        if (pins.i2cWriteBuffer(i2cgroveMultiswitch_x03, Buffer.fromArray([i2c_CMD_GET_DEV_EVENT])) != 0) {
-            n_Status_changed = (n_Status != eStatus.fehler)
+        if (n_Status == eStatus.fehler)
+            return true
+        else if (pins.i2cWriteBuffer(i2cGroveMultiswitch_x03, Buffer.fromArray([i2c_CMD_GET_DEV_EVENT])) != 0) {
+            n_Status_changed = (n_Status != eStatus.nicht_angeschlossen)
             n_Status = eStatus.fehler
             return false // i2c Fehler
         } else {
             n_enableButtonFunkgruppe = false
             n_Status_changed = false // wird nur bei Änderung true
 
-            let bu = pins.i2cReadBuffer(i2cgroveMultiswitch_x03, 10)
+            let bu = pins.i2cReadBuffer(i2cGroveMultiswitch_x03, 10)
             // Byte 0-3: 32 Bit UInt32LE; Byte 4:Schalter 1 ... Byte 9:Schalter 6
             // Byte 4-9: 00000001:Schalter OFF; 00000001:Schalter ON; Bit 1-7 löschen & 0x01
             for (let iSwitch = 1; iSwitch <= 5; iSwitch += 1) { // Richtung N = 1, W = 2, S = 3, O = 4, M = 5
@@ -43,12 +80,12 @@ namespace radio { // multiswitch.ts
 
                     switch (iSwitch) {
                         case eStatus.links: { // 2
-                            n_Status_changed = n_Magnet
+                            //n_Status_changed = n_Magnet
                             n_Magnet = false
                             break // beendet switch, nicht for
                         }
                         case eStatus.rechts: { // 4
-                            n_Status_changed = !n_Magnet
+                            //n_Status_changed = !n_Magnet
                             n_Magnet = true
                             break // beendet switch, nicht for
                         }
