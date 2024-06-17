e054235704f5bf7feddd5efb89e555e12e9cd496
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-06-17 16:43:46 +0200

    Aktualisiere main.blocks, led5x5.ts

--- led5x5.ts
+++ led5x5.ts
@@ -1,7 +1,55 @@
 
 namespace radio { // led5x5.ts
 
+
+    let n_showString = ""
+
+    //% group="25 LED" advanced=true color=#54C9C9
+    //% block="zeige Text wenn geändert %text" weight=1
+    export function zeigeText(text: any) {
+        let tx = convertToText(text)
+        if (tx != n_showString) {
+            n_showString = tx
+            basic.showString(tx)
+        }
+    }
+
+
     // zeigt Funkgruppe oder i²C Adresse im 5x5 Display binär an
+
+    export enum ePlot {
+        //% block="BIN 0..31"
+        bin,
+        //% block="HEX Zahl"
+        hex,
+        //% block="BCD Zahl"
+        bcd
+    }
+
+    //% group="25 LED" advanced=true color=#54C9C9
+    //% block="zeige binär %int %format ←x %x" weight=1
+    export function zeigeBIN(int: number, format: ePlot, x: number) {
+        int = Math.imul(int, 1) // 32 bit signed integer
+
+        if (format == ePlot.bin && between(x, 0, 4)) {
+            for (let y = 4; y >= 0; y--) {
+                if ((int % 2) == 1) { led.plot(x, y) } else { led.unplot(x, y) }
+                int = int >> 1 // bitweise Division durch 2
+            }
+        } else {
+            while (int > 0 && between(x, 0, 4)) {
+                if (format == ePlot.bcd) {
+                    plotBIN(int % 10, x)
+                    int = Math.idiv(int, 10) // 32 bit signed integer
+                } else if (format == ePlot.hex) {
+                    plotBIN(int % 16, x)
+                    int = int >> 4 // bitweise Division durch 16
+                }
+                x--
+            }
+        }
+    }
+
 
     //% group="25 LED" advanced=true
     //% block="BCD Zahl %int anzeigen ←x %x" weight=6
@@ -40,17 +88,7 @@ namespace radio { // led5x5.ts
         }
     }
 
-    let n_showString = ""
 
-    //% group="25 LED" advanced=true
-    //% block="zeige Text wenn geändert %text" weight=1
-    export function zeigeText(text: any) {
-        let tx = convertToText(text)
-        if (tx != n_showString) {
-            n_showString = tx
-            basic.showString(tx)
-        }
-    }
 
 
 
