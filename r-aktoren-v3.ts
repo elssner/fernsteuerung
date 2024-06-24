af394f276509f9afe35602799b1fa487c1756437
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-06-24 20:56:19 +0200

    Aktualisiere pxt.json, r-aktoren-v3.ts

--- r-aktoren-v3.ts
+++ r-aktoren-v3.ts
@@ -4,10 +4,10 @@ namespace receiver { // r-aktoren-v3.ts
     let n_rgbled = [0, 0, 0]
 
     //% group="Licht" subcategory="Aktoren"
-    //% block="RGB LEDs %led %color %on" weight=6
+    //% block="RGB LEDs3 %led %color %on" weight=6
     //% color.shadow="colorNumberPicker"
     //% on.shadow="toggleOnOff"
-    export function rgbLEDon(led: eRGBled, color: number, on: boolean) {
+    export function rgbLEDon3(led: eRGBled, color: number, on: boolean) {
         rgbLEDs(led, (on ? color : 0), false)
     }
 
