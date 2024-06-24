62d119de1ecc99c7ed812f9760c8a6bd4115b11c
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-06-24 17:42:01 +0200

    Aktualisiere main.blocks, s-sender.ts

--- s-sender.ts
+++ s-sender.ts
@@ -17,4 +17,42 @@ namespace sender {
         radio.setaktiviert(radio.radio_sendBuffer19(), radio.e3aktiviert.m0, true)
     }
 
+    //% group="radio.Sender" subcategory="Sender"
+    //% block="%buffer M0 Fahren M1 Gabelstapler A- B+ Lenken || 128 ± %p128 * %prozent \\%" weight=4
+    //% buffer.shadow="radio_sendBuffer19"
+    //% p128.min=0 p128.max=8 
+    //% prozent.min=10 prozent.max=100 prozent.defl=100
+    export function sendM01(buffer: Buffer, p128 = 0, prozent = 100) {
+        radio.setBetriebsart(radio.radio_sendBuffer19(), radio.e0Betriebsart.p0)
+        radio.setByte(radio.radio_sendBuffer19(), radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor, radio.motorProzent(joystickValue(eJoystickValue.xmotor, p128), prozent))
+        radio.setByte(radio.radio_sendBuffer19(), radio.eBufferPointer.m0, radio.eBufferOffset.b1_Servo, radio.getServoGabelstapler())
+        radio.setByte(radio.radio_sendBuffer19(), radio.eBufferPointer.m1, radio.eBufferOffset.b0_Motor, joystickValue(eJoystickValue.ymotor, p128))
+        radio.setaktiviert(radio.radio_sendBuffer19(), radio.e3aktiviert.m0, true)
+        radio.setaktiviert(radio.radio_sendBuffer19(), radio.e3aktiviert.m1, true)
+    }
+
+    //% group="radio.Sender" subcategory="Sender"
+    //% block="%buffer MA Seilrolle MB Drehkranz || 128 ± %p128" weight=3
+    //% buffer.shadow="radio_sendBuffer19"
+    //% p128.min=0 p128.max=8 
+    export function sendMAB(buffer: Buffer, p128 = 0) {
+        radio.setBetriebsart(radio.radio_sendBuffer19(), radio.e0Betriebsart.p0)
+        radio.setByte(radio.radio_sendBuffer19(), radio.eBufferPointer.ma, radio.eBufferOffset.b0_Motor, joystickValue(eJoystickValue.xmotor, p128))
+        radio.setByte(radio.radio_sendBuffer19(), radio.eBufferPointer.mb, radio.eBufferOffset.b0_Motor, joystickValue(eJoystickValue.ymotor, p128))
+        radio.setaktiviert(radio.radio_sendBuffer19(), radio.e3aktiviert.ma, true)
+        radio.setaktiviert(radio.radio_sendBuffer19(), radio.e3aktiviert.mb, true)
+    }
+
+    //% group="radio.Sender" subcategory="Sender"
+    //% block="%buffer MC Zahnstange MB Drehkranz || 128 ± %p128" weight=2
+    //% buffer.shadow="radio_sendBuffer19"
+    //% p128.min=0 p128.max=8 
+    export function sendMCB(buffer: Buffer, p128 = 0) {
+        radio.setBetriebsart(radio.radio_sendBuffer19(), radio.e0Betriebsart.p0)
+        radio.setByte(radio.radio_sendBuffer19(), radio.eBufferPointer.mc, radio.eBufferOffset.b0_Motor, joystickValue(eJoystickValue.xmotor, p128))
+        radio.setByte(radio.radio_sendBuffer19(), radio.eBufferPointer.mb, radio.eBufferOffset.b0_Motor, joystickValue(eJoystickValue.ymotor, p128))
+        radio.setaktiviert(radio.radio_sendBuffer19(), radio.e3aktiviert.mc, true)
+        radio.setaktiviert(radio.radio_sendBuffer19(), radio.e3aktiviert.mb, true)
+    }
+
 }
