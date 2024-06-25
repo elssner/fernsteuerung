efd9c614b490b2767249c198475da2d6358d7e36
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-06-25 19:50:35 +0200

    Aktualisiere main.blocks, s-sender.ts

--- s-sender.ts
+++ s-sender.ts
@@ -3,6 +3,37 @@
 namespace sender {
     //radio: color=#E3008C weight=96 icon="\uf012" groups='["Group", "Broadcast", "Send", "Receive"]'
     // BF3F7F
+
+
+
+
+    //% group="calliope-net.github.io/fernsteuerung"
+    //% block="beim Start || Funkgruppe %funkgruppe mit 'A- B+ halten' Funkgruppe ändern %bFunkgruppe" weight=8
+    //% funkgruppe.min=160 funkgruppe.max=191 funkgruppe.defl=175
+    //% bFunkgruppe.shadow="toggleYesNo"
+    //% inlineInputMode=inline
+    export function beimStart( funkgruppe = 175, bFunkgruppe = false) {
+        // n_ready = false // CaR4 ist nicht bereit: Schleifen werden nicht abgearbeitet
+/* 
+        pinRelay(true) // Relais an schalten
+
+        n_ServoGeradeaus = servoGeradeaus // Parameter
+        pins.servoWritePin(c_pinServo, n_ServoGeradeaus)
+
+        pins.setPull(c_pinEncoder, PinPullMode.PullUp) // Encoder PIN Eingang PullUp
+
+        //  n_ready = motorReset(ei2cMotor.i2cMotorAB) && motorReset(ei2cMotor.i2cMotorCD)
+        //if (qMotorReset())
+        qMotorReset() // true wenn qwiicmotor bereit, false wenn Kran nicht angeschlossen
+ */
+        // in Erweiterung fernsteuerung bluetooth.ts:
+        radio.beimStart(funkgruppe, bFunkgruppe) // setzt auch n_start true
+
+        //  addStatus(n_ready)
+    }
+
+
+
     // ========== group="Button A+B" subcategory="Sender"
 
     //% group="00 Sender" subcategory="Sender"
