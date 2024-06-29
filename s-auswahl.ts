41396aa6c4aba7215483e2a30f51e048ca65d13b
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-06-29 13:45:11 +0200

    Aktualisiere s-auswahl.ts

--- s-auswahl.ts
+++ s-auswahl.ts
@@ -1,46 +1,91 @@
 
 namespace sender { // s-auswahl.ts
 
+    let a_StorageBuffer = Buffer.create(4)
+
+    export enum eStorageBuffer { modell, b, c, d } // Index im Buffer
+
+    export enum eModelle {
+        cb2e,
+        mkcg,
+        mkck,
+        car4
+    } // so viele Images müssen im Array sein - Bilder am Ende dieser Datei
+
+  //  let n_Modell = eModelle.cb2e // aktuelles Modell, aus dem Flash und Buffer
+
+
+
+
+    //% group="Auswahl Modell" subcategory="Auswahl"
+    //% block="Start Auswahl Modell Flash einlesen %storagei32"
+    export function startAuswahl(storagei32: number) {
+        storageBufferSet(storagei32)
+        let iModell = a_StorageBuffer[eStorageBuffer.modell]
+        if (radio.between(iModell, 0, a_ModellImages.length - 1))
+            a_StorageBuffer[eStorageBuffer.modell] = iModell
+        else
+            a_StorageBuffer[eStorageBuffer.modell] = eModelle.cb2e
+
+        a_ModellImages[a_StorageBuffer[eStorageBuffer.modell]].showImage(0)
+    }
+
+
+    //% group="Auswahl Modell" subcategory="Auswahl"
+    //% block="Knopf A halten"
+    export function buttonA() {
+        if (a_StorageBuffer[eStorageBuffer.modell] > 0) {
+            a_StorageBuffer[eStorageBuffer.modell]--
+        }
+
+        a_ModellImages[a_StorageBuffer[eStorageBuffer.modell]].showImage(0)
+
+
+    }
+
+
+    //% group="Auswahl Modell" subcategory="Auswahl"
+    //% block="Knopf B halten "
+    export function buttonB() {
+        if (a_StorageBuffer[eStorageBuffer.modell] < a_ModellImages.length - 1) {
+            a_StorageBuffer[eStorageBuffer.modell]++
+        }
+       
+        a_ModellImages[a_StorageBuffer[eStorageBuffer.modell]].showImage(0)
+
+
+    }
+
 
 
     //% group="Auswahl Modell" subcategory="Auswahl"
     //% block="zeige Bild %index" weight=8
-    export function showImage(index: eImages) {
-        a_Images[index].showImage(0)
+    export function showImage(index: eModelle) {
+        a_ModellImages[index].showImage(0)
     }
 
 
     // ========== group="Storage (Flash)" color=#FFBB00
 
-    let n_StorageBuffer = Buffer.create(4)
-
-    enum eStorageBuffer { a, b, c, d } // Index im Buffer
-
     //% group="Storage (Flash)" subcategory="Auswahl"
     //% block="Flash einlesen %i32" weight=9
     export function storageBufferSet(i32: number) {
         // i32.shadow=storage_get_number
-        n_StorageBuffer.setNumber(NumberFormat.UInt32LE, 0, i32)
+        a_StorageBuffer.setNumber(NumberFormat.UInt32LE, 0, i32)
     }
 
     //% group="Storage (Flash)" subcategory="Auswahl"
     //% block="Flash speichern" weight=8
     export function storageBufferGet() {
-        return n_StorageBuffer.getNumber(NumberFormat.UInt32LE, 0)
+        return a_StorageBuffer.getNumber(NumberFormat.UInt32LE, 0)
     }
 
 
 
     // ========== Bilder für Auswahl Modell
 
-  export  enum eImages {
-        cb2e,
-        mkcg,
-        mkck,
-        car4,
-    }
 
-    let a_Images = [
+    let a_ModellImages = [
         images.createImage(`
     . # . # .
     . . . . .
@@ -68,14 +113,14 @@ namespace sender { // s-auswahl.ts
     # . . . #
     # # # # #
     . # . # .
-    `),
+    `)/* ,
         images.createImage(`
     . . . . .
     . # . # .
     # . . . #
     . # . # .
     . . . . .
-    `),
+    `), */
     ]
 
 } // s-auswahl.ts
