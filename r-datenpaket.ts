5c82d5207ef0b28fa6b264724253e6388fe02211
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-01 11:38:29 +0200

    Aktualisiere pxt.json, main.blocks, s-advanced.ts, r-datenpaket.ts

---
+++ r-datenpaket.ts
@@ -0,0 +1,29 @@
+
+namespace receiver { // r-datenpaket.ts
+
+    //% group="Betriebsart" subcategory="Datenpaket"
+    //% block="receivedData %buffer 00 Fernsteuerung Motoren" weight=5
+ 
+    export function sendM0(receivedData: Buffer) {
+
+        if (radio.isBetriebsart(receivedData, radio.e0Betriebsart.p0)) {
+            receiver.motor255(receiver.eMotor01.M0, radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor))
+            receiver.servo_set16(radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b1_Servo))
+            receiver.motor255(receiver.eMotor01.M1, radio.getByte(receivedData, radio.eBufferPointer.m1, radio.eBufferOffset.b0_Motor))
+            receiver.qMotorChipPower(receiver.eMotorChip.ab, radio.getaktiviert(receivedData, radio.e3aktiviert.ma) || radio.getaktiviert(receivedData, radio.e3aktiviert.mb))
+            receiver.qMotor255(receiver.eMotor.ma, radio.getByte(receivedData, radio.eBufferPointer.ma, radio.eBufferOffset.b0_Motor))
+            receiver.qMotor255(receiver.eMotor.mb, radio.getByte(receivedData, radio.eBufferPointer.mb, radio.eBufferOffset.b0_Motor))
+            receiver.qMotorChipPower(receiver.eMotorChip.cd, radio.getaktiviert(receivedData, radio.e3aktiviert.mc))
+            receiver.qMotor255(receiver.eMotor.mc, radio.getByte(receivedData, radio.eBufferPointer.mc, radio.eBufferOffset.b0_Motor))
+            receiver.qMotor255(receiver.eMotor.md, radio.getByte(receivedData, radio.eBufferPointer.md, radio.eBufferOffset.b0_Motor))
+
+            //receiver.ringTone(radio.getSchalter(receivedData, radio.e0Schalter.b0))
+            //receiver.qwiicRelay(radio.getSchalter(receivedData, radio.e0Schalter.b1))
+            //receiver.pinGPIO4(radio.getSchalter(receivedData, radio.e0Schalter.b2))
+            //receiver.rgbLEDs(receiver.eRGBled.a, 0x0000ff, true)
+        }
+
+    }
+
+
+} // r-datenpaket.ts
--- s-advanced.ts
+++ s-advanced.ts
@@ -27,34 +27,4 @@ namespace sender { // s-advanced.ts
     export function receivedStringText() { return n_receivedString.substr(2) }
 
 
-
-    // ========== group="lenken mit Tasten A:links B:rechts" advanced=true
-/* 
-    export enum eServoButton {
-        //% block="lesen"
-        lesen,
-        //% block="3° links"
-        links,
-        //% block="3° rechts"
-        rechts,
-        //% block="90° gerade"
-        gerade
-    }
-
-    let n_ServoButton = 16
-
-    //% group="lenken mit Tasten A:links B:rechts" advanced=true
-    //% block="Servo Winkel %i" weight=8
-    export function setServoButton(i: eServoButton): number {
-        if (i == eServoButton.links && n_ServoButton > 1)
-            n_ServoButton--
-        else if (i == eServoButton.rechts && n_ServoButton < 31)
-            n_ServoButton++
-        else if (i == eServoButton.gerade)
-            n_ServoButton = 16
-
-        return n_ServoButton
-    }
- */
-
 } // s-advanced.ts
