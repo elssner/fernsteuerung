26d1540c24baec6e8990cc24d59f3d3d60e32a6c
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-10 16:40:18 +0200

    Aktualisiere c-callibot.ts

--- c-callibot.ts
+++ c-callibot.ts
@@ -337,13 +337,13 @@ namespace cb2 { // c-callibot.ts 005F7F
 
 
     //% group="Programmieren" subcategory="Fahrstrecke"
-    //% block="fahre Motor (1 ↓ 128 ↑ 255) %motor Servo (1 ↖ 16 ↗ 31) %servo Strecke (cm) %strecke" weight=3
+    //% block="fahre Motor (1 ↓ 128 ↑ 255) %motor Servo (1 ↖ 16 ↗ 31) %servo Strecke (cm) %strecke || %a" weight=3
     // motor.min=0 motor.max=255 motor.defl=128
     //% motor.shadow=radio_speedPicker
     // servo.min=1 servo.max=31 servo.defl=16
     //% servo.shadow=radio_protractorPicker
     //% strecke.min=0 strecke.max=255 strecke.defl=20
-    export function fahreSchritt(motor: number, servo: number, strecke: number) {
+    export function fahreSchritt(motor: number, servo: number, strecke: number, a10?: number[]) {
 
         cb2.writeMotor128Servo16(c_MotorStop, servo)
         cb2.writeEncoderReset()
@@ -361,6 +361,8 @@ namespace cb2 { // c-callibot.ts 005F7F
         else {
             let i = Math.abs(Math.round(Math.map(motor, 1, 255, -9, 9)))
             let a = [160, 160, 91, 73, 63, 59, 56, 53, 52, 51] // Fahrzeit ms für 1cm bei 10%, 20% .. 100%
+            if (a10)
+                a = a10
             //  let t = input.runningTime()  // ms seit Start
             basic.showNumber(i)
             basic.pause(strecke * a[i])
